362add9d1401b7a3fe8deb8176f1c3e82efdf7a0
hasnat79 <hasnatabdullah79@gmail.com>
2024-12-06 00:07:38 -0600

    configs updated

---
+++ configs/configure.py
@@ -0,0 +1,22 @@
+
+# uag_oops dataset
+uag_oops_dataset_path = "data/uag_oops/oops_uag_paper_version.json"
+uag_oops_video_dir = "data/uag_oops/videos"
+
+llama3_model_id = "src/checkpoints/meta-llama/Meta-Llama-3-8B-Instruct"
+
+#ssbd dataset
+ssbd_data_path = "data/uag_ssbd/ssbd_paper_version.json" 
+ssbd_video_dir = "data/uag_ssbd/videos"
+# funQA dataset
+funqa_data_path = "data/uag_funqa/uag_funqa_dataset.json"
+funqa_test_humor_video_dir = "data/uag_funqa/videos"
+
+# charades dataset
+charades_sta_test_path = "data/charades_sta/Charades_STA_test.json"
+charades_video_dir ="data/charades_sta/videos/"
+
+# videochatgpt
+llava_lightning_7b_v1_1_path = "foundation_models/Video-ChatGPT/LLaVA-7B-Lightening-v1-1"
+video_chatgpt_weights_path = "foundation_models/Video-ChatGPT/video_chatgpt-7B.bin"
+
